506b636459b2eeabee5c5de53de289c7e5b64cad
Kevin McIntyre <kebin70@gmail.com>
2025-02-01 14:56:50 -0800

    add playwright and jimp

---
+++ app/image-converter/page.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Image Converter",
+  };
+
+export default function Home() {
+    return (<div>
+      <input type="file" id="file" accept="image/*" />
+      <select>
+        <option value="png">PNG</option>
+        <option value="jpg">JPG</option>
+        <option value="webp">WEBP</option>
+      </select>
+    </div>)
+}
